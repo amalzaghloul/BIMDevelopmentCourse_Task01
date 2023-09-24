9e998ece81755dac42c9ad580eeec2c72f3f6743
hello <hello@DESKTOP-A1NIV2N>
2023-09-25 02:02:25 +0300

    Add project files.

---
+++ Data Type/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region Data Types
+
+            //// As a structural engineer 
+            ////Calculate the total weight of steel beams  
+
+
+            //Console.WriteLine("please Calculate the total weight of steel beams");
+            //double length = 5.2;
+            //double weight = 7.2;
+            //double totalWeight = length * weight;
+
+            //Console.WriteLine(totalWeight);
+
+            //Console.WriteLine("------------------------------------");
+
+
+
+
+            //// As an Architectural Engineer
+            ////indicate if a room is accessible to disable individuals.
+
+            //bool accessibleForDisabledPeople = true;
+            //Console.WriteLine(accessibleForDisabledPeople);
+            //Console.ReadLine();
+
+
+            //Console.WriteLine("------------------------------------");
+
+
+            //// As an MEP engineer 
+            ////assign unique codes to electrical panels  
+
+            //char panel01 = 'A';
+            //char panel02 = 'x';
+            //Console.WriteLine("please give codes to electrical panels ");
+            //Console.WriteLine(panel02);
+            //Console.ReadLine();
+
+
+            //Console.WriteLine("------------------------------------");
+            #endregion
+
+            #region Operators
+
+            // As a structural engineer 
+            //Determine the column's capacity load 
+
+            int Fcu = 25;
+            int fy = 360;
+            
+            double columnWidth = 250;
+        
+            double columnDepth = 600;
+            double Ac = columnDepth * columnWidth;
+            double As = 0.01 * Ac;
+            Console.WriteLine("please calculate the PU");
+            Console.WriteLine(0.35 * Ac * Fcu + 0.67 * As * fy);
+            Console.ReadLine();
+
+
+            Console.WriteLine("------------------------------------");
+
+
+            // As an Architectural Engineer
+            //estimate the cost of materials .
+
+            int costEPG_m = 1000;
+            double arae = 12.6;
+            Console.WriteLine("please calculate the total cost");
+            double totalCost = costEPG_m * arae;
+            Console.WriteLine(totalCost);
+
+
+            Console.ReadLine();
+
+
+            Console.WriteLine("------------------------------------");
+
+
+            // As an MEP engineer 
+            //Design an electrical circuit for a room  
+
+            int numOfBulbs = 3;
+            int powerConsump_ = 60;
+            Console.WriteLine("please calculate the total power consumption ");
+            int totalPowerConsump_ = numOfBulbs * powerConsump_;
+            Console.WriteLine(totalPowerConsump_);
+           
+            Console.ReadLine();
+
+
+            Console.WriteLine("------------------------------------");
+            #endregion
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ task 1/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variables
+
+            //// As a structural engineer 
+            ////Represent the number of floors 
+
+            //int numOfFloors = 11;
+            //Console.WriteLine("please represent the number of floors");
+            //Console.WriteLine(numOfFloors);
+
+            //Console.WriteLine("------------------------------------");
+
+
+
+
+            //// As an Architectural Engineer
+            ////keep track of the dimensions of different rooms 
+
+            //double length = 3.6;
+            //double width = 4.2;
+            //int heigh = 3;
+            //Console.WriteLine("please give the value of length , width , heigh of the room");
+            //Console.WriteLine(length);
+            //Console.WriteLine(width);
+            //Console.WriteLine(heigh);
+            //Console.ReadLine();
+
+
+            //Console.WriteLine("------------------------------------");
+
+
+            //// As an MEP engineer 
+            ////Manage the energy consumption of a building 
+
+            //string system01 = "Ductwork";
+            //string system02 = "Thermostat";
+            //Console.WriteLine("please give the current HVAC Systems names");
+            //Console.WriteLine(system01);
+            //Console.WriteLine(system02);
+            //Console.ReadLine();
+
+
+            //Console.WriteLine("------------------------------------"); 
+            #endregion
+
+            #region Data Types
+
+            // As a structural engineer 
+            //Calculate the total weight of steel beams  
+
+
+            Console.WriteLine("please Calculate the total weight of steel beams");
+            double length = 5.2;
+            double weight = 7.2;
+            double totalWeight = length * weight;
+
+            Console.WriteLine(totalWeight);
+
+            Console.WriteLine("------------------------------------");
+
+
+
+
+            //// As an Architectural Engineer
+            ////keep track of the dimensions of different rooms 
+
+            //double length = 3.6;
+            //double width = 4.2;
+            //int heigh = 3;
+            //Console.WriteLine("please give the value of length , width , heigh of the room");
+            //Console.WriteLine(length);
+            //Console.WriteLine(width);
+            //Console.WriteLine(heigh);
+            //Console.ReadLine();
+
+
+            //Console.WriteLine("------------------------------------");
+
+
+            //// As an MEP engineer 
+            ////Manage the energy consumption of a building 
+
+            //string system01 = "Ductwork";
+            //string system02 = "Thermostat";
+            //Console.WriteLine("please give the current HVAC Systems names");
+            //Console.WriteLine(system01);
+            //Console.WriteLine(system02);
+            //Console.ReadLine();
+
+
+            //Console.WriteLine("------------------------------------");
+            #endregion
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
